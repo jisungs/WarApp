16187441555bdfe55664d1440a575c6412005ee0
jisung <jisung.smart@gmail.com>
2018-08-05 09:36:21 +0900

    Add code into the Action.
    Add code show card changes. The function uses random func becaus it need to change number everytime push "deal" button.

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -28,7 +28,14 @@ class ViewController: UIViewController {
 
     @IBAction func dealTapped(_ sender: Any) {
         
-        print("Deal!")
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        //print("left random number is \(leftRandomNumber)")
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        //print("rightn random number is \(rightRandomNumber)")
+        
+        leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
+        
     }
     
 
