b62f01774ea67aba5a42b5939acb0c49d799a835
jisung <jisung.smart@gmail.com>
2018-08-05 09:16:06 +0900

    Add outlet and function
    To act proper function add outlet and function.

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        print("Deal!")
+    }
+    
 
 }
 
